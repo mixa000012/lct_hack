ba3f1f92217f4057b182c2e55dbbed18d5fb1100
Shira <shirokoffson@mail.ru>
2023-05-28 23:23:26 +0300

    small fix

--- api/endpoints.py
+++ api/endpoints.py
@@ -82,9 +82,9 @@ async def get_group_from_db(group_id: int, session: AsyncSession) -> Groups:
 
 @groups_router.post("/groups")
 async def read_group(
-        group_id: list[int],
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user_from_token),
+    group_id: list[int],
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
 ) -> list[Group]:
     """
     Asynchronously retrieves a list of Group objects specified by their IDs. The returned Group objects also include
@@ -194,7 +194,7 @@ async def get_address(coordinates: str) -> dict:
 
 @recs_router.get("/")
 async def give_recs(
-        current_user: User = Depends(get_current_user_from_token),
+    current_user: User = Depends(get_current_user_from_token),
 ) -> list[int]:
     """
     Asynchronously generates recommendations for the current user based on their user data.
@@ -216,7 +216,7 @@ async def give_recs(
 
 @recs_router.post("/new")
 async def give_recs_for_new_users(
-        current_user: User = Depends(get_current_user_from_token),
+    current_user: User = Depends(get_current_user_from_token),
 ) -> list[int]:
     """
     Asynchronously generates recommendations for new users based on their survey results and other data.
@@ -242,7 +242,7 @@ async def give_recs_for_new_users(
 
 @recs_router.get("/is_exist")
 async def is_exist_recs(
-        current_user: User = Depends(get_current_user_from_token),
+    current_user: User = Depends(get_current_user_from_token),
 ) -> bool:
     """
     Asynchronously checks if the current user has any recommendations available.
@@ -266,9 +266,9 @@ async def is_exist_recs(
 
 @groups_router.get("/group")
 async def get_group(
-        group_name: str,
-        current_user: User = Depends(get_current_user_from_token),
-        db: AsyncSession = Depends(get_db),
+    group_name: str,
+    current_user: User = Depends(get_current_user_from_token),
+    db: AsyncSession = Depends(get_db),
 ) -> list[Group]:
     """
     Asynchronously retrieves a list of Group objects that match the provided group name. It also includes
@@ -321,9 +321,9 @@ async def get_group(
 
 @groups_router.post("/attends")
 async def create_attend(
-        group_id: int,
-        current_user: User = Depends(get_current_user_from_token),
-        db: AsyncSession = Depends(get_db),
+    group_id: int,
+    current_user: User = Depends(get_current_user_from_token),
+    db: AsyncSession = Depends(get_db),
 ):
     """
     Asynchronously creates an attendance record for the current user for a specified group.
@@ -374,9 +374,9 @@ async def create_attend(
 
 @groups_router.delete("/attends/{id}")
 async def delete_attends(
-        id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user_from_token),
+    id: int,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
 ):
     """
     Asynchronously deletes an attendance record specified by its identifier.
@@ -409,9 +409,11 @@ async def delete_attends(
     return {"detail": "Deleted successfully"}
 
 
-@groups_router.get("/attends/{id}")
-async def get_attends_by_id(current_user: User = Depends(get_current_user_from_token),
-                            db: AsyncSession = Depends(get_db)) -> list[AttendShow]:
+@groups_router.get("/attends_user")
+async def get_attends_by_id(
+    current_user: User = Depends(get_current_user_from_token),
+    db: AsyncSession = Depends(get_db),
+) -> list[AttendShow]:
     attends = await db.execute(select(Attends).where(Attends.id == current_user.id))
     attends = attends.scalars().all()
     attends_show = []
@@ -425,7 +427,7 @@ async def get_attends_by_id(current_user: User = Depends(get_current_user_from_t
             Offline=i.Offline,
             date=i.date,
             start=i.start,
-            end=i.end
+            end=i.end,
         )
         attends_show.append(attend)
 
