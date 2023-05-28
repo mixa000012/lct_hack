8b170bdf9f4942dc070a5d4bfff4d9fab149fa8e
Shira <shirokoffson@mail.ru>
2023-05-28 17:19:38 +0300

    add endpoint

--- main.py
+++ main.py
@@ -5,18 +5,32 @@ from fastapi.routing import APIRouter
 from api.endpoints import recs_router
 from api.endpoints import routes_router
 from api.user import user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="lct_hack")
 
 main_api_router = APIRouter()
 
-main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+origins = [
+    "http://lapki.itatmisis.ru",
+    "https://lapki.itatmisis.ru",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+main_api_router.include_router(user_router, prefix="/api/v1/user", tags=["user"])
 main_api_router.include_router(routes_router, prefix="/api/v1/routes", tags=["routes"])
-main_api_router.include_router(recs_router, prefix="/recs", tags=["recs"])
+main_api_router.include_router(recs_router, prefix="/api/v1/recs", tags=["recs"])
 
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="10.0.0.51", port=8080,
-                ssl_keyfile="etc/letsencrypt/live/api.lapki.itatmisis.ru/fullchain.pem",
-                ssl_certfile="etc/letsencrypt/live/api.lapki.itatmisis.ru/privkey.pem")
+                ssl_keyfile="/etc/letsencrypt/live/api.lapki.itatmisis.ru/privkey.pem",
+                ssl_certfile="/etc/letsencrypt/live/api.lapki.itatmisis.ru/fullchain.pem")
