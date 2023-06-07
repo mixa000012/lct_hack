7d2ce3a7e3b7ae511f9c20e7fff41b2f98614a1b
Shira <shirokoffson@mail.ru>
2023-06-07 20:16:07 +0300

    fix lk

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,5 +1,6 @@
 import ast
 from datetime import datetime
+from random import randint
 
 import openrouteservice
 from fastapi import APIRouter
@@ -11,7 +12,7 @@ from openrouteservice.geocode import pelias_reverse
 from openrouteservice.geocode import pelias_search
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from random import randint
+
 from api.actions.auth import get_current_user_from_token
 from api.schemas import AttendShow
 from api.schemas import Group
@@ -82,9 +83,9 @@ async def get_group_from_db(group_id: int, session: AsyncSession) -> Groups:
 
 @groups_router.post("/groups")
 async def read_group(
-        group_id: list[int],
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user_from_token),
+    group_id: list[int],
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
 ) -> list[Group]:
     """
     Asynchronously retrieves a list of Group objects specified by their IDs. The returned Group objects also include
@@ -194,7 +195,7 @@ async def get_address(coordinates: str) -> dict:
 
 @recs_router.get("/")
 async def give_recs(
-        current_user: User = Depends(get_current_user_from_token),
+    current_user: User = Depends(get_current_user_from_token),
 ) -> list[int]:
     """
     Asynchronously generates recommendations for the current user based on their user data.
@@ -216,7 +217,7 @@ async def give_recs(
 
 @recs_router.post("/new")
 async def give_recs_for_new_users(
-        current_user: User = Depends(get_current_user_from_token),
+    current_user: User = Depends(get_current_user_from_token),
 ) -> list[int]:
     """
     Asynchronously generates recommendations for new users based on their survey results and other data.
@@ -246,7 +247,7 @@ async def give_recs_for_new_users(
 
 @recs_router.get("/is_exist")
 async def is_exist_recs(
-        current_user: User = Depends(get_current_user_from_token),
+    current_user: User = Depends(get_current_user_from_token),
 ) -> bool:
     """
     Asynchronously checks if the current user has any recommendations available.
@@ -270,9 +271,9 @@ async def is_exist_recs(
 
 @groups_router.get("/group")
 async def get_group(
-        group_name: str,
-        current_user: User = Depends(get_current_user_from_token),
-        db: AsyncSession = Depends(get_db),
+    group_name: str,
+    current_user: User = Depends(get_current_user_from_token),
+    db: AsyncSession = Depends(get_db),
 ) -> list[Group]:
     """
     Asynchronously retrieves a list of Group objects that match the provided group name. It also includes
@@ -337,9 +338,9 @@ async def check_attend(group_id: int, user_id: int, db):
 
 @groups_router.post("/attends")
 async def create_attend(
-        group_id: int,
-        current_user: User = Depends(get_current_user_from_token),
-        db: AsyncSession = Depends(get_db),
+    group_id: int,
+    current_user: User = Depends(get_current_user_from_token),
+    db: AsyncSession = Depends(get_db),
 ):
     """
     Asynchronously creates an attendance record for the current user for a specified group.
@@ -374,7 +375,7 @@ async def create_attend(
         direction_3=group.direction_3,
         Offline=False if group.closest_metro == "Онлайн" else True,
         date=await get_date(),
-        start=group.schedule_active,
+        start=group.schedule_planned,
         end=group.schedule_active,
         metro=group.closest_metro,
         address=group.address,
@@ -399,9 +400,9 @@ async def create_attend(
 
 @groups_router.delete("/attends/{id}")
 async def delete_attends(
-        id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user_from_token),
+    id: int,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
 ):
     """
     Asynchronously deletes an attendance record specified by its identifier.
@@ -436,8 +437,8 @@ async def delete_attends(
 
 @groups_router.get("/attends_user")
 async def get_attends_by_id(
-        current_user: User = Depends(get_current_user_from_token),
-        db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+    db: AsyncSession = Depends(get_db),
 ) -> list[AttendShow]:
     attends = await db.execute(
         select(Attends).where(Attends.user_id == current_user.id)
