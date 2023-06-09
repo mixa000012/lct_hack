3c2417aed0b9b307b60c22b3a593498487690af7
Shira <shirokoffson@mail.ru>
2023-06-09 18:17:35 +0300

    reorganized give_recs

--- api/endpoints.py
+++ api/endpoints.py
@@ -107,9 +107,9 @@ async def get_group_from_db(group_id: int, session: AsyncSession) -> Groups:
 
 @groups_router.post("/groups")
 async def read_group(
-    group_id: list[int],
-    db: AsyncSession = Depends(get_db),
-    current_user: User = Depends(get_current_user_from_token),
+        group_id: list[int],
+        db: AsyncSession = Depends(get_db),
+        current_user: User = Depends(get_current_user_from_token),
 ) -> list[Group]:
     """
     Asynchronously retrieves a list of Group objects specified by their IDs. The returned Group objects also include
@@ -220,9 +220,9 @@ async def get_address(coordinates: str) -> dict:
 
 @recs_router.get("/")
 async def give_recs(
-    current_user: User = Depends(get_current_user_from_token),
-    db: AsyncSession = Depends(get_db),
-    is_new: bool = False,
+        current_user: User = Depends(get_current_user_from_token),
+        db: AsyncSession = Depends(get_db),
+        is_new: bool = False,
 ) -> list[int]:
     """
     Asynchronously generates recommendations for the current user based on their user data.
@@ -242,7 +242,7 @@ async def give_recs(
         result = current_user.ml_result
         return ast.literal_eval(result)
 
-    if is_new:
+    if is_new or current_user.survey_result:
         if current_user.survey_result is None:
             raise HTTPException(status_code=400, detail="No survey results")
         now_interests = ast.literal_eval(current_user.survey_result)
@@ -267,7 +267,7 @@ async def give_recs(
 
 @recs_router.get("/is_recs_exist")
 async def is_recs_exist(
-    current_user: User = Depends(get_current_user_from_token),
+        current_user: User = Depends(get_current_user_from_token),
 ) -> bool:
     """
     Asynchronously checks if the current user has any recommendations available.
@@ -293,9 +293,9 @@ async def is_recs_exist(
 
 @groups_router.get("/group")
 async def get_group(
-    group_name: str,
-    current_user: User = Depends(get_current_user_from_token),
-    db: AsyncSession = Depends(get_db),
+        group_name: str,
+        current_user: User = Depends(get_current_user_from_token),
+        db: AsyncSession = Depends(get_db),
 ) -> list[Group]:
     """
     Asynchronously retrieves a list of Group objects that match the provided group name. It also includes
@@ -360,9 +360,9 @@ async def check_attend(group_id: int, user_id: int, db):
 
 @groups_router.post("/attends")
 async def create_attend(
-    group_id: int,
-    current_user: User = Depends(get_current_user_from_token),
-    db: AsyncSession = Depends(get_db),
+        group_id: int,
+        current_user: User = Depends(get_current_user_from_token),
+        db: AsyncSession = Depends(get_db),
 ):
     """
     Asynchronously creates an attendance record for the current user for a specified group.
@@ -428,9 +428,9 @@ async def create_attend(
 
 @groups_router.delete("/attends/{id}")
 async def delete_attends(
-    id: int,
-    db: AsyncSession = Depends(get_db),
-    current_user: User = Depends(get_current_user_from_token),
+        id: int,
+        db: AsyncSession = Depends(get_db),
+        current_user: User = Depends(get_current_user_from_token),
 ):
     """
     Asynchronously deletes an attendance record specified by its identifier.
@@ -465,8 +465,8 @@ async def delete_attends(
 
 @groups_router.get("/attends_user")
 async def get_attends_by_id(
-    current_user: User = Depends(get_current_user_from_token),
-    db: AsyncSession = Depends(get_db),
+        current_user: User = Depends(get_current_user_from_token),
+        db: AsyncSession = Depends(get_db),
 ) -> list[AttendShow]:
     attends = await db.execute(
         select(Attends).where(Attends.user_id == current_user.id)
